0c096786eb57b2e7b6fe9f055d4303b32588643b
caio <caiovitordias1@gmail.com>
2024-11-04 20:55:58 -0300

    feat: finalizada nova tela de login

--- src/Pages/NovoLogin/NovoLogin.tsx
+++ src/Pages/NovoLogin/NovoLogin.tsx
@@ -13,6 +13,8 @@ export default function Login() {
     const [senha, setSenha] = useState("");
     const [nome, setNome] = useState("");
     const [dataNascimento, setDataNascimento] = useState(""); // Novo estado para data de nascimento
+    const [emailEntrar, setEmailEntrar] = useState("");
+    const [senhaEntrar, setSenhaEntrar] = useState("");
 
     const [mostrarCadastro, setMostrarCadastro] = useState(false);
     const handleCadastroClick = () => setMostrarCadastro(true);
@@ -25,9 +27,9 @@ export default function Login() {
 
     const navigate = useNavigate();
 
-    // Função para navegar para a página de login
+    // Função para navegar para a página de home
     function handleNavigation() {
-        navigate("/entrar");
+        navigate("/");
     }
 
     // Função para cadastro
@@ -45,13 +47,40 @@ export default function Login() {
             const response = await axios.post(urlLoginCadastro, objetoUserParaCadastrar);
 
             if (response.status === 201) { // Verifica se há um token   
-                navigate("/entrar");
+                navigate("/login");
             } else {
                 window.alert("Cadastro falhou. Verifique as informações fornecidas.");
             }
         } catch (error) {
             console.error("Erro ao realizar o cadastro: ", error);
             window.alert("Não foi possível realizar o cadastro");
+        }
+    }
+    async function handleSubmit() {
+        const urlLogin = "http://localhost:3002/login"; // URL da API
+        const objetoUser = {
+            email: emailEntrar,
+            senha: senhaEntrar
+        };
+
+        try {
+            const response = await axios.post(urlLogin, objetoUser);
+            console.log(response.data);
+            
+            if (response.status === 200 && response.data.token) { // Verifica se login foi bem-sucedido e há um token
+                const token = response.data.token;
+
+                // Salvar token no localStorage (ou sessionStorage, se preferir)
+                localStorage.setItem('Token', token);
+
+                // Navega para a página "home"
+                navigate("/");
+            } else {
+                window.alert("Login falhou. Verifique suas credenciais.");
+            }
+        } catch (error) {
+            console.error("Erro ao fazer login: ", error);
+            window.alert("Não foi possível realizar o login");
         }
     }
 
@@ -65,28 +94,30 @@ export default function Login() {
                     <div className="lado-esquerdo">
                       
                         
-                        <h2 className="texto-bemvindo">Entrer na sua conta</h2>
-                        <img src={logoTexto} className="palavra-logo" alt="Logo"/>
+                        <h2 className="texto-bemvindo">Entre na sua conta</h2>
+                        <div className="posicaologo">
+                        <img src={logoTexto} className="palavra-logo" id="palavra-logo-esquerdo" alt="Logo"/>
+                        </div>
                         <div className="bloco-inputs-novo">
                         
                         <input
                             className="inputs-entrar-novo"
                             type="text"
                             placeholder="Email..."
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
+                            value={emailEntrar}
+                            onChange={e => setEmailEntrar(e.target.value)}
                         />
                         
                         <input  
                             className="inputs-entrar-novo"
                             type="password"
                             placeholder="Senha..."
-                            value={senha}
-                            onChange={e => setSenha(e.target.value)}
+                            value={senhaEntrar}
+                            onChange={e => setSenhaEntrar(e.target.value)}
                         />
                     </div>
                     
-                        <button className="botao-entrar" onClick={handleNavigation}>Entrar</button>
+                        <button className="botao-entrar" onClick={handleSubmit}>Entrar</button>
                  
                     </div>
                     <div className="lado-direito">
@@ -124,11 +155,12 @@ export default function Login() {
                             value={dataNascimento} 
                             onChange={e => setDataNascimento(e.target.value)}
                         />
-
-                        <button className="botao-cadastrar" onClick={handleCadastro}>Cadastrar</button>
+                        <div className="posicionarbcadastrar">
+                        <button className="botao-cadastrar"  onClick={handleCadastro}>Cadastrar</button>
+                        </div>
                     </div>
                     <div className={`clique-esquerdo ${mostrarCadastro ? 'ativo' : ''}`} id={"clique-esquerdo"}>
-                        <h2>Bem vindo de volta!</h2>
+                        <h2 className="titulo-cliqueesquerdo">Bem vindo de volta!</h2>
                         <div className="texto2-cliqueesquerdo">
                             <h3>Entre na sua conta para poder acessar seu histórico de imagens dentre outras
                             funcionalidades</h3>
@@ -137,13 +169,19 @@ export default function Login() {
                         <button onClick={handleToggle}>
                             Entrar
                         </button>
+                        <div className="botaoconvidado">
+                        <button id="bconvidado" onClick={handleNavigation}>
+                            Entrar como convidado
+                        </button>
+                        </div>
                         </div>
                     </div>
 
                     <div className={`clique-direito ${!mostrarCadastro ? 'ativo' : ''}`}>
-                        <h2>Bem vindo!</h2>
+                        <h2 className="titulo-cliquedireito">Bem vindo!</h2>
                         <div className="texto2-cliquedireito">
                             <h3>A melhor solução para tratamento de nuvens!</h3>
+                            <h3>Visualize e baixe imagens captadas pelo satélite CBERS4A com o sensor WPM!</h3>
                         </div>
                         <div className="botaodireitocadastrar">
                             <button onClick={handleToggle}>
